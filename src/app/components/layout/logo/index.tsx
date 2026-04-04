178a367d87da13107ae8c32309a99de1b0c54669
MANOJ-CM2026 <manojanbu70@gmail.com>
2026-04-04 20:06:37 +0530

    Remove header text logo

--- src/app/components/layout/logo/index.tsx
+++ src/app/components/layout/logo/index.tsx
@@ -3,13 +3,7 @@ import Link from "next/link";
 const Logo = () => {
   return (
     <>
-      <Link href="/" className="flex items-center gap-2 group">
-        <div className="flex items-center justify-center w-10 h-10 bg-primary text-black font-bold text-xl rounded-full group-hover:scale-105 transition-transform duration-300">
-          M
-        </div>
-        <span className="text-xl font-bold tracking-wide">
-          Manoj Anbalagan
-        </span>
+      <Link href="/">
       </Link>
     </>
   );
